374cc81d4aeacc917f1729af7449c22e24102e9e
peihua <379046428@qq.com>
2023-12-09 09:42:27 +0800

    优化代码

--- lib/constants.dart
+++ lib/constants.dart
@@ -1,9 +1,3 @@
-// Copyright 2019 The Flutter team. All rights reserved.
-// Use of this source code is governed by a BSD-style license that can be
-// found in the LICENSE file.
-
-// Only put constants shared between files here.
-
 import 'dart:typed_data';
 
 import 'package:transparent_image/transparent_image.dart' as transparent_image;
--- lib/pages/settings/settings.dart
+++ lib/pages/settings/settings.dart
@@ -1,10 +1,5 @@
-// Copyright 2019 The Flutter team. All rights reserved.
-// Use of this source code is governed by a BSD-style license that can be
-// found in the LICENSE file.
-
 import 'dart:collection';
 
-import 'package:chatbox_flutter/data/entry.dart';
 import 'package:chatbox_flutter/widgets/SinglePickerWidget.dart';
 import 'package:collection/collection.dart';
 import 'package:flutter/material.dart';
